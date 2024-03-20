75e5537036fc9019996a20aad11156c1ea626a1c
winpigface <winpigface2537@gmail.com>
2024-03-20 17:58:32 +0700

    Version 3-Data_type

---
+++ 2_data_type.ts
@@ -0,0 +1,27 @@
+// Primative types (basic types): number , string , boolean
+// Basic operators: + - * /
+// Comparison operators: > < = >= <= != !==
+
+// Number
+console.log(1);
+console.log(-1);
+console.log(0.123);
+console.log(100 + 5 + 6 + 5);
+// String
+console.log("Hello World");
+console.log("1");
+console.log("1 + 1");
+console.log("Single quote");
+console.log("Double quote 'This's single quote'");
+console.log(`Back quote`);
+console.log("Message 1" + "Message 2");
+console.log("100" + "5" + "6" + "5");
+console.log(100 + "5");
+// Boolean
+console.log(true);
+console.log(false);
+console.log(1 > 0);
+console.log(1 < 0);
+console.log(0 == 0);
+console.log(1 == "1");
+console.log(1 === "1");
